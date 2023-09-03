6d0d46f476d419a2a0c497d090e58fc177936d73
Julio Perez <A01705763@tec.mx>
2023-09-03 12:49:55 -0600

    Con este commit se agregaron comentarios al codigo en la clase coin_change_avaro para explicar el funcionamiento y la complejidad del codigo, tambien se agrego comentarios al main para explicar el funcionamiento

--- A15_Dinamica_Avaro_Equipo_2/coin_change_avaro.h
+++ A15_Dinamica_Avaro_Equipo_2/coin_change_avaro.h
@@ -1,15 +1,36 @@
+/*  
+    Programa para entregrar el cambio usando un metodo Avaro
+
+    El programa resive las monedas validas, la entrada y el cambio que se debe de dar.
+
+    Daniel Gutiérrez Gómez A01068056
+    Juan Manuel González Ascencio A00572003
+    Julio César Pérez Rodríguez A01705763
+    Creación 30/08/23, Modificación 31/08/23
+*/
+
 #include <iostream>
 #include <map>
 #include "coin_change_dp.h"
 
-
+/*
+    Este codigo tiene una complejidad de O(n log m), debido a que el primer ciclo 
+    tiene una complejidad de O(n) donde n es la cantidad de monedas que tenemos 
+    disponibles, y el segundo ciclo tiene una complejidad de O(log m) donde m es 
+    la cantidad de cambio que tiene que dar en base al valor de las monedas. 
+*/
 int avaro(int monedas[], int entradas, int cambio){
+    //Este contador los usaremos para imprimir las monedas que vamos a usar
     int monedasUsadas = 0;
     std::map<int, int> cantidadMonedas;
     int i = entradas - 1;
     for (; i >= 0; i--){
+        /*Comprobamos el cambio con el tamaño de la moneda, para entregar
+        las monedas mas grandes primero y para luego completar las que siguen
+        en valor y asi hasta completar el cambio.*/
         while (cambio >= monedas[i]){
             cambio -= monedas[i];
+            //Sumamos la cantidad de monedas que estamos usando
             monedasUsadas++;
             cantidadMonedas[monedas[i]]++;
         }
--- A15_Dinamica_Avaro_Equipo_2/main.cpp
+++ A15_Dinamica_Avaro_Equipo_2/main.cpp
@@ -1,3 +1,13 @@
+/*  
+    Este programa utiliza las clases coin_change_dp y coin_change_avaro
+    para mostrar los resultados al entregar cambio con los diferentes 
+    métodos, el avaro y el dinámico.  
+
+    Daniel Gutiérrez Gómez A01068056
+    Juan Manuel González Ascencio A00572003
+    Julio César Pérez Rodríguez A01705763
+    Creación 30/08/23, Modificación 31/08/23
+*/
 #include "coin_change_dp.h"
 #include "coin_change_avaro.h"
 #include <iostream>
@@ -8,9 +18,11 @@ int main(int argc, char *argv[]){
     std::vector<int> nums;
     int N;
     int i = 0;
+    //Ingresamos los valores 
     std::cout << "Dame numero de monedas que quieres procesar ";
     std::cin >> N;
     int monedasAvaro[N];
+    //Obtenemos los valores de las monedas
     while (N){
         int temp;
         std::cout << "Dame valor de moneda: ";
@@ -20,17 +32,20 @@ int main(int argc, char *argv[]){
         N--;
     }
     int P, Q;
+    //Obtenemos el costo y el pago para calcular el cambio
     std::cout << "Dame costo del producto: ";
     std::cin >> P;
     std::cout << "Tu pago: ";
     std::cin >> Q;
     std::sort(nums.begin(), nums.end());
     std::sort(monedasAvaro, monedasAvaro + i);
+    //Calculamos el numero de monedas que vamos a utilizar con el metodo Avaro    
     std::cout << "Avaro: ";
     int ansAvaro = avaro(monedasAvaro, i, Q - P);
     std::cout << "Cantidad de monedas algoritmo avaro: " << ansAvaro;
     std::cout << std::endl;
     std::cout << std::endl;
+    //Calculamos el numero de monedas que vamos a utilizar con el metodo Dinamico  
     std::cout << "Programacion dinamica: ";
     int ans = dynammicCoinChange(nums, P, Q);
     std::cout << "Cantidad minima de monedas para dar cambio fueron: " << ans;
