e330180f45f784d523ccee1f94a0b3e729d13372
Dani Gutiérrez <a01068056@tec.mx>
2023-09-23 21:31:18 -0600

    Feat(algorithm): Added First Part

--- SituaciónProblema1/readFiles.cpp
+++ SituaciónProblema1/readFiles.cpp
@@ -7,6 +7,17 @@
 using namespace std;
 vector<pair<vector<string>, vector<string>>> files;
 
+bool isPalindrome(int left, int right, string mcode) {
+    while(left <= right) {
+        if(mcode[left] != mcode[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
 void createStringsToRead(vector<string> &files, string extension){
   for (int i = 0; i < 7; i++){
     int temp = 1;
@@ -17,59 +28,64 @@ void createStringsToRead(vector<string> &files, string extension){
   }
 }
 
-vector<string> leerArchivo(vector<string> transmissions){
+void createStringsToReadTransmission(vector<string> &files, string extension){
+  for (int i = 0; i < 7; i++){
+    int temp = 1;
+    while (temp <= 2){
+      files.push_back(extension + to_string(i) + to_string(temp) + ".txt");
+      temp++;
+    }
+  }
+}
 
+vector<string> leerArchivo(vector<string> transmissions){
   vector<string> transmission;
 
-  for(int i=0; i<transmissions.size(); i++){
+  for(int i = 0 ; i < transmissions.size(); i++){
     string longitudArchivo;
     string archivo = transmissions[i];
     
     ifstream inFile(archivo);
     string line;
-    while (getline(inFile, line))
-    {
+    while (getline(inFile, line)){
         longitudArchivo += line;
     }
     transmission.push_back(longitudArchivo);
-    cout<<transmission[i];
   }
   return transmission;
 }
 
-void kmpApplication(vector<string> transmissions, vector<int> lpsTables, vector<string> target) {
-    
-    for (int x=0; x<lpsTables.size();x++){
-
-      for (int y=0; y<transmissions.size();y++){
-        cout << "entra al segundo for"<<endl;
-        int i =0;
-        int j =0;
-        string temp = transmissions[j];
-        
-        while(i<=temp.length()){
-        cout<<lpsTables[x];
-          if (target[x][j]==transmissions[y][i]){
+void kmpApplication(vector<string> transmissions, vector<vector<int>> lpsTables, vector<string> target) {
+    for (int x = 0; x < transmissions.size(); x++){ // Iterar dos veces para cada target
+      for (int y = 0; y < target.size(); y++){ // Iterar tres veces para cada transmision
+        int i = 0;
+        int j = 0;
+        string temp = transmissions[x];
+        while(i < temp.length()){
+            // Comparación mcode con transmision char por char
+          if (target[y][j] == transmissions[x][i]){
             i++;
             j++;
           }
-
-          if (j==target[x].length()-1){
-            j=0;
-            cout<<"se encontro la palabra en la pos "<<i-target[x].length()<< ", "<<i<<endl;
+          // Comparación si j llegó a la longitud de la palabra de mcode
+          if (j == target[y].length()){
+            j = 0;
+            cout << "Se encontró la palabra: " << target[y] << " en la pos " << (i - target[y].length()) + 1 << ", " << i + 1 << endl;
+            cout << "En el archivo de transmisión: " << x + 1;
+            cout << endl;
           }
-          else if (j>0){
-            j=lpsTables[x];
-          }
-          else{
-            i++;
+          // j se va hacia atrás
+          else if (i < temp.length() && target[y][j] != transmissions[x][i]){
+            if (j != 0) {
+                j = lpsTables[y][j - 1];
             }
-          
+            else {
+                i++;
+            }
+          }
         }
       }
-
     }
-
 }
 
 void makePairsVectorFiles(vector<string> mcodes, vector<string> transcodes){
@@ -104,11 +120,11 @@ void makeLpsTable(string mcode, vector<int> &lpsTable){
       lpsTable[i] = j;
       i++;
     }
-    else{
+    else {
       if (j != 0){
         j = lpsTable[j - 1];
       }
-      else{
+      else {
         lpsTable[i] = 0;
         i++;
       }
@@ -128,42 +144,42 @@ int main(int argc, char *argv[]){
   string transTemp = "transmission/transmission";
   vector<string> mcodeFiles;
   vector<string> transFiles;
+
   createStringsToRead(mcodeFiles, mcodeTemp);
-  createStringsToRead(transFiles, transTemp);
+  createStringsToReadTransmission(transFiles, transTemp);
   makePairsVectorFiles(mcodeFiles, transFiles);
   for (int i = 0; i < files.size(); i++){
     vector<string> firstMcode = files[i].first;
     vector<string> firstTrans = files[i].second;
+    vector<vector<int>> lpsTables;
+    vector<string> transmissions;
+    vector<string> mcodes;
     for (int j = 0; j < firstMcode.size(); j++){
       ifstream mCode(firstMcode[j]);
       if (mCode.is_open()){
         string temp;
         while (mCode.good()){
           mCode >> temp;
-          cout << temp;
         }
-
+        mcodes.push_back(temp);
         vector<int> lps(temp.size());
-        vector<string> transmissions;
         makeLpsTable(temp, lps);
-        transmissions = leerArchivo(firstTrans);
+        lpsTables.push_back(lps);
 
-        kmpApplication(transmissions, lps, firstMcode);
-        
-        for (int x = 0; x < lps.size(); x++){
-          cout << lps[x];
+        if (!isPalindrome(0, temp.size() - 1, temp)) {
+          reverse(temp.begin(), temp.end());
+          mcodes.push_back(temp);
+          makeLpsTable(temp, lps);
+          lpsTables.push_back(lps);
         }
         mCode.close();
-        cout << endl;
       }
-      else{
-        std::cerr << "Unable to open file." << std::endl;
+      else {
+        cerr << "Unable to open file." << endl;
       }
     }
+    transmissions = leerArchivo(firstTrans);
+    kmpApplication(transmissions, lpsTables, mcodes);
     cout << endl;
-
   }
-
-
-
 }
