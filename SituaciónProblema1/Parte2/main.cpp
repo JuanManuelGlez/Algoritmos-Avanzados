99fd712a32b59d53e20f50f942c677860bb7f0d7
Dani Gutiérrez <a01068056@tec.mx>
2023-09-25 17:28:54 -0600

    Feat(algorithm): Added Third Part

--- SituaciónProblema1/Parte2/main.cpp
+++ SituaciónProblema1/Parte2/main.cpp
@@ -1,12 +1,21 @@
 #include <vector>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <algorithm>
+#include <utility>
+#include "../readFiles.h"
 
 using namespace std;
 
-int longestCommonSubstring( vector<vector<int>> tabulator, string subs1, string subs2) {
+vector<pair<string, string>> files;
+
+vector<pair<int, int>> longestCommonSubstring(vector<vector<int>> tabulator,
+                                              string subs1, string subs2) {
+    vector<pair<int, int>> indices;
     int maxInt = 0;
+    int endIndex = 0;
+    vector<int> lengthSubs(max(subs1.size(), subs2.size()));
     for(int row = 0; row < tabulator.size(); row ++) {
         for(int col = 0; col < tabulator[0].size(); col++) {
             if(col == 0 || row == 0){
@@ -20,20 +29,54 @@ int longestCommonSubstring( vector<vector<int>> tabulator, string subs1, string 
                 }
                 if(maxInt < tabulator[row][col]) {
                     maxInt = tabulator[row][col];
+                    endIndex = col - 1;
                 }
             }
         }
+        if (maxInt > 0){
+            int startIndex = (endIndex - maxInt + 1) + 1;
+            indices.emplace_back(startIndex, endIndex + 1);
+            maxInt = 0;
+        }
     }
-    return maxInt;
+    return indices;
+}
+
+void leerArchivoTrans(string &transmission, string transFile) {
+    ifstream inFile(transFile);
+    string line;
+    while (getline(inFile, line)) {
+        transmission += line;
+    }
 }
 
 int main(int argc, char *argv[]) {
-    string subs1 = "baaxcd";
-    string subs2 = "baaxd";
-
-    vector<vector<int>> tabulator(subs2.size(), vector<int>(subs1.size()));
-
-    int max = longestCommonSubstring(tabulator, subs1, subs2);
-    cout << max;
-
+    string trans = "../transmission/";
+    vector<string> transmissions;
+    createStringsToReadTransmission(transmissions, "transmission");
+    for(int i = 0; i < transmissions.size(); i+=2) {
+        files.emplace_back(transmissions[i], transmissions[i + 1]);
+    }
+    // Aplicar comparación de strings para cada par de files leídos
+    for(int i = 0; i < files.size(); i++) {
+        string subs1;
+        string subs2;
+        leerArchivoTrans(subs1, trans + files[i].first);
+        leerArchivoTrans(subs2, trans + files[i].second);
+        vector<vector<int>> tabulator(subs2.size(), vector<int>(subs1.size()));
+        vector<pair<int, int>> indices = longestCommonSubstring(tabulator, subs1, subs2);
+        int diff = 0;
+        int start = 0;
+        int end = 0;
+        for(int x = 0; x < indices.size(); x++){
+            if (diff <= (indices[x].second - indices[x].first)){
+                diff = indices[x].second - indices[x].first;
+                start = indices[x].first;
+                end = indices[x].second;
+            }
+        }
+        cout << "En archivo: " << files[i].first + ".txt";
+        cout << " El substring común más largo empieza en: " << start;
+        cout << " termina en: " << end << endl;
+    }
 }
