872eadd39570ce1c5e4d4db598999fd68939ac5c
Dani Gutiérrez <a01068056@tec.mx>
2023-09-30 12:32:47 -0600

    fix(struct): Added gitignore and minor fix to indeces

--- SituaciónProblema1/Parte1/kmpApplication.h
+++ SituaciónProblema1/Parte1/kmpApplication.h
@@ -17,28 +17,34 @@ using namespace std;
  * mcode
  * Time complexity O(n) donde n es longitud de mcode
  */
-void makeLpsTable(string mcode, vector<int> &lpsTable){
-    int j = 0;
+void makeLpsTable(string mcode, vector<int> &lpsTable)
+{
+  int j = 0;
 
-    lpsTable[0] = 0;
+  lpsTable[0] = 0;
 
-    int i = 1;
-    while (i < mcode.length()){
-        if (mcode[i] == mcode[j]){
-            j++;
-            lpsTable[i] = j;
-            i++;
-        }
-        else{
-            if (j != 0){
-                j = lpsTable[j - 1];
-            }
-            else{
-                lpsTable[i] = 0;
-                i++;
-            }
-        }
+  int i = 1;
+  while (i < mcode.length())
+  {
+    if (mcode[i] == mcode[j])
+    {
+      j++;
+      lpsTable[i] = j;
+      i++;
     }
+    else
+    {
+      if (j != 0)
+      {
+        j = lpsTable[j - 1];
+      }
+      else
+      {
+        lpsTable[i] = 0;
+        i++;
+      }
+    }
+  }
 }
 
 /**
@@ -54,52 +60,62 @@ void makeLpsTable(string mcode, vector<int> &lpsTable){
  */
 void kmpApplication(vector<string> transmissions,
                     vector<vector<int>> lpsTables, vector<string> target, int posFile,
-                    vector<string> mcodeFiles) {
-    for (int x = 0; x < transmissions.size(); x++){ // Iterar dos veces para cada transmssion
-        for (int y = 0; y < target.size(); y++){ // Iterar tres veces para cada mcode
-            // Apuntadores para hacer comparaciones con algoritmo KMP
-            int i = 0;
-            int j = 0;
-            int k = 0;
-            // String temporal de la transmisión
-            string temp = transmissions[x];
-            while (i < temp.length()){
-                // Comparación mcode con transmision char por char
-                // si son iguales, aumentar en
-                if (target[y][j] == transmissions[x][i]){
-                    i++;
-                    j++;
-                }
-                // Comparación si j llegó a la longitud de la palabra de mcode
-                // setear j a 0, porque puede haber más apariciones
-                if (j == target[y].length()) {
-                    k++;
-                    j = 0;
-                    cout << "(True) En la posicion inicial: "
-                         << (i - target[y].length()) + 1
-                         << " y final: " << i + 1;
-                    cout << " en el archivo de transmission" << posFile << x + 1 << ".txt";
-                    cout << " en archivo: " << mcodeFiles[y].substr(7);
-                    cout << endl;
-                }
-                // Si la i todavía no llega al final del string y no hay match de caracteres
-                else if (i < temp.length() && target[y][j] != transmissions[x][i]){
-                    // Si j no es 0, retrasar a j, porque puede existir un prefijo
-                    if (j != 0){
-                        j = lpsTables[y][j - 1];
-                    }
-                    // Si no, avanzar en i
-                    else{
-                        i++;
-                    }
-                }
-            }
-            if (k < 1) {
-                cout << "(False) El archivo transmission" << posFile << x + 1 << ".txt";
-                cout << " no contiene el codigo contenido en el archivo: " << mcodeFiles[y].substr(7) << endl;
-            }
+                    vector<string> mcodeFiles)
+{
+  for (int x = 0; x < transmissions.size(); x++)
+  { // Iterar dos veces para cada transmssion
+    for (int y = 0; y < target.size(); y++)
+    { // Iterar tres veces para cada mcode
+      // Apuntadores para hacer comparaciones con algoritmo KMP
+      int i = 0;
+      int j = 0;
+      int k = 0;
+      // String temporal de la transmisión
+      string temp = transmissions[x];
+      while (i < temp.length())
+      {
+        // Comparación mcode con transmision char por char
+        // si son iguales, aumentar en
+        if (target[y][j] == transmissions[x][i])
+        {
+          i++;
+          j++;
         }
+        // Comparación si j llegó a la longitud de la palabra de mcode
+        // setear j a 0, porque puede haber más apariciones
+        if (j == target[y].length())
+        {
+          k++;
+          j = 0;
+          cout << "(True) En la posicion inicial: "
+               << (i - target[y].length()) + 1
+               << " y final: " << i;
+          cout << " en el archivo de transmission" << posFile << x + 1 << ".txt";
+          cout << " en archivo: " << mcodeFiles[y].substr(7);
+          cout << endl;
+        }
+        // Si la i todavía no llega al final del string y no hay match de caracteres
+        else if (i < temp.length() && target[y][j] != transmissions[x][i])
+        {
+          // Si j no es 0, retrasar a j, porque puede existir un prefijo
+          if (j != 0)
+          {
+            j = lpsTables[y][j - 1];
+          }
+          // Si no, avanzar en i
+          else
+          {
+            i++;
+          }
+        }
+      }
+      if (k < 1)
+      {
+        cout << "(False) El archivo transmission" << posFile << x + 1 << ".txt";
+        cout << " no contiene el codigo contenido en el archivo: " << mcodeFiles[y].substr(7) << endl;
+      }
     }
+  }
 }
 
 /**
@@ -110,20 +126,23 @@ void kmpApplication(vector<string> transmissions,
  * @return bool isPalindrome or not
  * Time complexity O(n)
  */
-bool isPalindrome(int left, int right, string mcode){
-    // Mientras apuntador izquierdo
-    // sea menor al derecho, revisar
-    // si los chars son diferentes, en ese
-    // caso, regresar false, de otra manera
-    // aumentar apuntadores
-    while (left <= right){
-        if (mcode[left] != mcode[right]){
-            return false;
-        }
-        left++;
-        right--;
+bool isPalindrome(int left, int right, string mcode)
+{
+  // Mientras apuntador izquierdo
+  // sea menor al derecho, revisar
+  // si los chars son diferentes, en ese
+  // caso, regresar false, de otra manera
+  // aumentar apuntadores
+  while (left <= right)
+  {
+    if (mcode[left] != mcode[right])
+    {
+      return false;
     }
-    return true;
+    left++;
+    right--;
+  }
+  return true;
 }
 
 #endif
