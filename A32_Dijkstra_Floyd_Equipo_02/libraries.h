20ec1ac4a0eb15c767ed6b542abaf4c43e2f3692
Dani Gutiérrez <a01068056@tec.mx>
2023-10-20 12:44:56 -0600

    fix(libraries): Fixes to libraries

---
+++ A32_Dijkstra_Floyd_Equipo_02/libraries.h
@@ -0,0 +1,12 @@
+// mylibraries.h
+
+#ifndef MYLIBRARIES_H
+#define MYLIBRARIES_H
+
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <climits>
+
+#endif // MYLIBRARIES_H
